cf609435f1678a00bd640cb07ffa3ebeaddbfff2
Prabhu Subramanian <prabhu@appthreat.com>
2020-01-15 10:24:45 +0000

    Added tests

--- lib/context.py
+++ lib/context.py
@@ -1,42 +1,55 @@
+import logging
 import os
 
 from git import Repo
 
+import lib.config as config
 
-def find_repo_details(src_dir):
+LOG = logging.getLogger(__name__)
+
+
+def find_repo_details(src_dir=None):
     """Method to find repo details such as url, sha etc
     This will be populated into versionControlProvenance attribute
 
     :param src_dir: Source directory
     """
-    repositoryUri = ""
+    # See if repository uri is specified in the config
+    repositoryUri = config.get("repository_uri")
     revisionId = ""
     branch = ""
     """
-    # Since CI servers typically checkout repo in detached mode, we need to rely on environment
-    # variables as a starting point to find the repo details
-    # Env variables detection for popular CI server
-    # Azure pipelines - https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
-    # GitHub actions - https://help.github.com/en/actions/automating-your-workflow-with-github-actions/using-environment-variables
-    # Google CloudBuild - https://cloud.google.com/cloud-build/docs/configuring-builds/substitute-variable-values
-    # CircleCI - https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
-    # Travis - https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
-    # AWS CodeBuild - https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-env-vars.html
+    Since CI servers typically checkout repo in detached mode, we need to rely on environment
+    variables as a starting point to find the repo details. To make matters worse, since we
+    run the tools inside a container these variables should be passed as part of the docker run
+    command. With native integrations such as GitHub action and cloudbuild this could be taken
+    care by our builders.
+
+    Env variables detection for popular CI server is implemented here anyways. But they are effective
+    only in few cases.
+
+    Azure pipelines - https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
+    GitHub actions - https://help.github.com/en/actions/automating-your-workflow-with-github-actions/using-environment-variables
+    Google CloudBuild - https://cloud.google.com/cloud-build/docs/configuring-builds/substitute-variable-values
+    CircleCI - https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
+    Travis - https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+    AWS CodeBuild - https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-env-vars.html
     """
     for key, value in os.environ.items():
         # Check REPOSITORY_URL first followed CI specific vars
         # Some CI such as GitHub pass only the slug instead of the full url :(
+        if not repositoryUri:
+            if key in [
+                "REPOSITORY_URL",
+                "BUILD_REPOSITORY_URI",
+                "GITHUB_REPOSITORY",
+                "REPO_NAME",
+                "CIRCLE_REPOSITORY_URL",
+                "TRAVIS_REPO_SLUG",
+                "CODEBUILD_SOURCE_REPO_URL",
+            ]:
+                repositoryUri = value
         if key in [
-            "REPOSITORY_URL",
-            "BUILD_REPOSITORY_URI",
-            "GITHUB_REPOSITORY",
-            "REPO_NAME",
-            "CIRCLE_REPOSITORY_URL",
-            "TRAVIS_REPO_SLUG",
-            "CODEBUILD_SOURCE_REPO_URL",
-        ]:
-            repositoryUri = value
-        elif key in [
             "COMMIT_SHA",
             "BUILD_SOURCEVERSION",
             "GITHUB_SHA",
@@ -45,7 +58,7 @@ def find_repo_details(src_dir):
             "CODEBUILD_SOURCE_VERSION",
         ]:
             revisionId = value
-        elif key in [
+        if key in [
             "BRANCH",
             "BUILD_SOURCEBRANCH",
             "GITHUB_REF",
@@ -54,18 +67,21 @@ def find_repo_details(src_dir):
             "TRAVIS_BRANCH",
         ]:
             branch = value
-    # Try interacting with git
-    try:
-        repo = Repo(src_dir)
-        if not branch:
-            branch = repo.active_branch.name
-        if not revisionId:
-            head = repo.heads[0]
-            revisionId = head.commit.hexsha
-        if not repositoryUri:
-            repositoryUri = next(iter(repo.remote().urls))
-    except Exception as e:
-        print(e)
+    if src_dir:
+        # Try interacting with git
+        try:
+            repo = Repo(src_dir)
+            if not branch:
+                branch = repo.active_branch.name
+            if not revisionId:
+                head = repo.heads[0]
+                revisionId = head.commit.hexsha
+            if not repositoryUri:
+                repositoryUri = next(iter(repo.remote().urls))
+        except:
+            LOG.warn(
+                "Unable to find repo details from the local repository. Consider adding a local .sastscanrc file with the url details."
+            )
 
     # Cleanup the variables
     branch = branch.replace("refs/heads/", "")
--- test/test_context.py
+++ test/test_context.py
@@ -7,3 +7,14 @@ def test_find_repo():
     curr_rep_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
     repo_details = context.find_repo_details(curr_rep_dir)
     assert len(repo_details.keys()) == 3
+
+
+def test_env_detection():
+    os.environ["BUILD_REPOSITORY_URI"] = "https://foo.com/bar"
+    os.environ["BUILD_SOURCEVERSION"] = "123"
+    os.environ["BUILD_SOURCEBRANCH"] = "develop"
+
+    repo_details = context.find_repo_details(None)
+    assert repo_details["repositoryUri"] == "https://foo.com/bar"
+    assert repo_details["revisionId"] == "123"
+    assert repo_details["branch"] == "develop"
