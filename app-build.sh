4eed6c1d6004571d35687304c9c06f6afbd3df2f
Prabhu Subramanian <prabhu@shiftleft.io>
2020-07-07 23:06:18 +0100

    Latest composer (#118)
    * Latest composer
    
    * Latest composer

--- app-build.sh
+++ app-build.sh
@@ -2,5 +2,8 @@
 
 rm -rf AppDir appimage-builder-cache
 rm *.AppImage*
+mkdir -p appimage-builder-cache
+cp ~/Downloads/runtime-x86_64 appimage-builder-cache/
 UPDATE_INFO="gh-releases-zsync|ShiftLeftSecurity|sast-scan|latest|*x86_64.AppImage.zsync" appimage-builder --recipe appimage-builder.yml --skip-test
 rm -rf AppDir appimage-builder-cache
+chmod +x *.AppImage
