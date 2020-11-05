5bf9b43357944169a73809f2ddf83fe74a866ac2
Prabhu Subramanian <prabhu@shiftleft.io>
2020-11-05 19:38:55 +0000

    Remove callproc to reduce FP

--- lib/logger.py
+++ lib/logger.py
@@ -29,7 +29,11 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(message)s",
     datefmt="[%X]",
-    handlers=[RichHandler(console=console, markup=True, show_path=False, enable_link_path=False)],
+    handlers=[
+        RichHandler(
+            console=console, markup=True, show_path=False, enable_link_path=False
+        )
+    ],
 )
 LOG = logging.getLogger(__name__)
 
