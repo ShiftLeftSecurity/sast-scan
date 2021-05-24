5cf1cfa01036a05999622feeed03118f1caa422c
Prabhu Subramanian <prabhu@shiftleft.io>
2021-05-24 19:58:46 +0000

    container image scan feature

--- dev-build.sh
+++ dev-build.sh
@@ -3,9 +3,9 @@ DOCKER_CMD=docker
 if command -v podman >/dev/null 2>&1; then
     DOCKER_CMD=podman
 fi
-python3 -m black .
-python3 -m black scan
 isort **/*.py
+python3 -m black lib
+python3 -m black scan
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --exit-zero --statistics
 
