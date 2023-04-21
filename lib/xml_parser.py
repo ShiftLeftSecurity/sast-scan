a4c6e6f21435e34c6bcc2e4dcc6e70682b8bce54
prabhu <prabhu@appthreat.com>
2023-04-21 18:24:30 +0200

    Hey Apache-2.0 (#389)
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- lib/xml_parser.py
+++ lib/xml_parser.py
@@ -1,18 +1,3 @@
-# This file is part of Scan.
-
-# Scan is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Scan is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Scan.  If not, see <https://www.gnu.org/licenses/>.
-
 import os
 
 from defusedxml.ElementTree import parse
