fc623512591949347f1b3abfd9e308722a4b87b6
Horacio Duran <horacio@qwiet.ai>
2023-08-15 11:54:08 +0200

    Add aarch64 support (#392)
    Add aarch64 (arm64) support to existing scripts where possible and create aarch64 versions where not possible or unclear/prone to error.
    
    Ensure scripts are a bit more verbose where useful and that they exit on error.
    
    Refactor some steps for clarity.
    
    This does not take care of possible maintenance of the existing features only adds new architecture support to be on par with others.

--- appimage-reqs.sh
+++ appimage-reqs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -xe
+#ARCH=arm64 # This should be set in the builder docker file
 APPDIR=$1
 OPTDIR=${APPDIR}/opt
 GOSEC_VERSION=2.14.0
@@ -15,38 +17,59 @@ SB_VERSION=4.7.3
 NODE_VERSION=14.5.0
 export PATH=$PATH:${APPDIR}/usr/bin:
 
-curl -LO "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz" \
-    && tar -C ${APPDIR}/usr/bin/ -xvf node-v${NODE_VERSION}-linux-x64.tar.xz \
-    && mv ${APPDIR}/usr/bin/node-v${NODE_VERSION}-linux-x64 ${APPDIR}/usr/bin/nodejs \
+NODE_TAR=node-v${NODE_VERSION}-linux-${ARCH}.tar.gz
+
+curl -LO "https://nodejs.org/dist/v${NODE_VERSION}/${NODE_TAR}" \
+    && tar -C ${APPDIR}/usr/bin/ -xzvf ${NODE_TAR} \
+    && mv ${APPDIR}/usr/bin/node-v${NODE_VERSION}-linux-${ARCH} ${APPDIR}/usr/bin/nodejs \
     && chmod +x ${APPDIR}/usr/bin/nodejs/bin/node \
     && chmod +x ${APPDIR}/usr/bin/nodejs/bin/npm \
-    && rm node-v${NODE_VERSION}-linux-x64.tar.xz
-curl -LO "https://github.com/securego/gosec/releases/download/v${GOSEC_VERSION}/gosec_${GOSEC_VERSION}_linux_amd64.tar.gz" \
-    && tar -C ${APPDIR}/usr/bin/ -xvf gosec_${GOSEC_VERSION}_linux_amd64.tar.gz \
+    && rm node-v${NODE_VERSION}-linux-x64.tar.gz
+
+GOSEC_TAR="gosec_${GOSEC_VERSION}_linux_${ARCH}.tar.gz"
+curl -LO "https://github.com/securego/gosec/releases/download/v${GOSEC_VERSION}/${GOSEC_TAR}" \
+    && tar -C ${APPDIR}/usr/bin/ -xzvf ${GOSEC_TAR} \
     && chmod +x ${APPDIR}/usr/bin/gosec \
-    && rm gosec_${GOSEC_VERSION}_linux_amd64.tar.gz
-curl -LO "https://github.com/dominikh/go-tools/releases/download/${SC_VERSION}/staticcheck_linux_amd64.tar.gz" \
-    && tar -C /tmp -xvf staticcheck_linux_amd64.tar.gz \
+    && rm ${GOSEC_TAR}
+
+STCHECK_TAR="staticcheck_linux_${ARCH}.tar.gz"
+curl -LO "https://github.com/dominikh/go-tools/releases/download/${SC_VERSION}/${STCHECK_TAR}" \
+    && tar -C /tmp -xzvf ${STCHECK_TAR} \
     && chmod +x /tmp/staticcheck/staticcheck \
     && cp /tmp/staticcheck/staticcheck ${APPDIR}/usr/bin/staticcheck \
-    && rm staticcheck_linux_amd64.tar.gz
-curl -L "https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks-linux-amd64" -o "${APPDIR}/usr/bin/gitleaks" \
+    && rm ${STCHECK_TAR}
+
+GLEAKS_TAR="gitleaks-linux-${ARCH}"
+TFSEC_TAR="tfsec-linux-${ARCH}"
+curl -L "https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/${GLEAKS_TAR}" -o "${APPDIR}/usr/bin/gitleaks" \
     && chmod +x ${APPDIR}/usr/bin/gitleaks \
-    && curl -L "https://github.com/aquasecurity/tfsec/releases/download/v${TFSEC_VERSION}/tfsec-linux-amd64" -o "${APPDIR}/usr/bin/tfsec" \
+    && curl -L "https://github.com/aquasecurity/tfsec/releases/download/v${TFSEC_VERSION}/${TFSEC_TAR}" -o "${APPDIR}/usr/bin/tfsec" \
     && chmod +x ${APPDIR}/usr/bin/tfsec
-curl -L "https://github.com/zegl/kube-score/releases/download/v${KUBE_SCORE_VERSION}/kube-score_${KUBE_SCORE_VERSION}_linux_amd64" -o "${APPDIR}/usr/bin/kube-score" \
+
+K8SCORE_TAR="kube-score_${KUBE_SCORE_VERSION}_linux_${ARCH}"
+curl -L "https://github.com/zegl/kube-score/releases/download/v${KUBE_SCORE_VERSION}/${K8SCORE_TAR}" -o "${APPDIR}/usr/bin/kube-score" \
     && chmod +x ${APPDIR}/usr/bin/kube-score \
     && wget "https://github.com/pmd/pmd/releases/download/pmd_releases%2F${PMD_VERSION}/pmd-bin-${PMD_VERSION}.zip" \
     && unzip -q pmd-bin-${PMD_VERSION}.zip -d ${OPTDIR}/ \
     && rm pmd-bin-${PMD_VERSION}.zip \
     && mv ${OPTDIR}/pmd-bin-${PMD_VERSION} ${OPTDIR}/pmd-bin
+
+if [ "$ARCH" = "x86_64" ]; then
+    KUBE_ARCH="amd64"
+else
+    KUBE_ARCH="$ARCH"
+fi
+
+K8SSEC_TAR="kubesec_linux_${KUBE_ARCH}.tar.gz"
 curl -L "https://github.com/detekt/detekt/releases/download/v${DETEKT_VERSION}/detekt-cli-${DETEKT_VERSION}-all.jar" -o "${APPDIR}/usr/bin/detekt-cli.jar" \
-    && curl -LO "https://github.com/controlplaneio/kubesec/releases/download/v${KUBESEC_VERSION}/kubesec_linux_amd64.tar.gz" \
-    && tar -C ${APPDIR}/usr/bin/ -xvf kubesec_linux_amd64.tar.gz \
-    && rm kubesec_linux_amd64.tar.gz \
+    && curl -LO "https://github.com/controlplaneio/kubesec/releases/download/v${KUBESEC_VERSION}/${K8SSEC_TAR}" \
+    && tar -C ${APPDIR}/usr/bin/ -xzvf ${K8SSEC_TAR} \
+    && rm ${K8SSEC_TAR} \
     && curl -LO "https://github.com/spotbugs/spotbugs/releases/download/${SB_VERSION}/spotbugs-${SB_VERSION}.tgz" \
-    && tar -C ${OPTDIR}/ -xvf spotbugs-${SB_VERSION}.tgz \
+    && file spotbugs-${SB_VERSION}.tgz \
+    && tar -C ${OPTDIR}/ -xzvf spotbugs-${SB_VERSION}.tgz \
     && rm spotbugs-${SB_VERSION}.tgz
+
 curl -LO "https://repo1.maven.org/maven2/com/h3xstream/findsecbugs/findsecbugs-plugin/${FSB_VERSION}/findsecbugs-plugin-${FSB_VERSION}.jar" \
     && mv findsecbugs-plugin-${FSB_VERSION}.jar ${OPTDIR}/spotbugs-${SB_VERSION}/plugin/findsecbugs-plugin.jar \
     && curl -LO "https://repo1.maven.org/maven2/com/mebigfatguy/sb-contrib/sb-contrib/${SB_CONTRIB_VERSION}/sb-contrib-${SB_CONTRIB_VERSION}.jar" \
